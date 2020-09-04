ae40bd07448f9f27bd327e02480ccf4ed4a887c9
Matt <matthew.westover@gmail.com>
2020-09-04 13:40:08 -0600

    Add Calculate BMR

--- WAMApp/app/src/main/java/com/example/wamapp/User.java
+++ WAMApp/app/src/main/java/com/example/wamapp/User.java
@@ -38,10 +38,31 @@ public class User {
         double heightSq = height * height;
         double weightConv = weight * 703;
         double BMI = weightConv / heightSq;
-        return BMI; 
+        return BMI;
     }
 
-
+    // Calculate BMR
+    // Formula from: https://bmi-calories.com/bmr-calculator.html
+    // Men BMR = 88.362 + (13.397 x weight in kg) + (4.799 x height in cm) - (5.677 x age in years)
+    // Women BMR = 447.593 + (9.247 x weight in kg) + (3.098 x height in cm) - (4.330 x age in years)
+    public static double calculateBMR(int height, double weight, int age, Sex sex){
+        double heightCM = height * 2.54;
+        double weightKG = weight * 0.453592;
+        double BMR = 0;
+        if(sex == Sex.MALE){
+            weightKG = weightKG * 13.397;
+            heightCM = heightCM * 4.799;
+            double age2 = age * 5.677;
+            BMR = 88.382 + weightKG + heightCM - age2;
+        } else {
+            weightKG = weightKG * 9.247;
+            heightCM = heightCM * 3.098;
+            double age2 = age * 4.33;
+            BMR = 447.593 + weightKG + heightCM - age2;
+        }
+        return BMR;
+    }
+    
     // Getters and Setters
     public int getUserID() {
         return userID;
