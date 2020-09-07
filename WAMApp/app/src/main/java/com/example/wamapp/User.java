6b0c1bbb10097b3b550823fd036a750791616438
Matt <matthew.westover@gmail.com>
2020-09-07 15:54:18 -0600

    Corrections and calorieCalc function added.

--- WAMApp/app/src/main/java/com/example/wamapp/User.java
+++ WAMApp/app/src/main/java/com/example/wamapp/User.java
@@ -6,26 +6,25 @@ public class User {
     private String firstName;
     private String lastName;
     private int age;
-    private Sex sex;
+    private String sex;
     private String photoPath;
-    private int zipCode;
+    private String city;
     private String country;
     private int height; // convert to total inches
     private double weight; // convert as total pounds
     private int BMI;
     private int BMR;
-    private ActiveLevel activeLevel;
+    private String activeLevel;
     private int weightGoal;
     private int calories;
 
-
-    public User(String firstName, String lastName, int age, Sex sex, String photoPath, int zipCode, String country, int height, double weight) {
+    public User(String firstName, String lastName, int age, String sex, String photoPath, String city, String country, int height, double weight) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.sex = sex;
         this.photoPath = photoPath;
-        this.zipCode = zipCode;
+        this.city = city;
         this.country = country;
         this.height = height;
         this.weight = weight;
@@ -45,11 +44,11 @@ public class User {
     // Formula from: https://bmi-calories.com/bmr-calculator.html
     // Men BMR = 88.362 + (13.397 x weight in kg) + (4.799 x height in cm) - (5.677 x age in years)
     // Women BMR = 447.593 + (9.247 x weight in kg) + (3.098 x height in cm) - (4.330 x age in years)
-    public static double calculateBMR(int height, double weight, int age, Sex sex){
+    public static double calculateBMR(int height, double weight, int age, String sex){
         double heightCM = height * 2.54;
         double weightKG = weight * 0.453592;
         double BMR = 0;
-        if(sex == Sex.MALE){
+        if(sex == "Male"){
             weightKG = weightKG * 13.397;
             heightCM = heightCM * 4.799;
             double age2 = age * 5.677;
@@ -61,6 +60,22 @@ public class User {
             BMR = 447.593 + weightKG + heightCM - age2;
         }
         return BMR;
+    }
+
+    public static double calculateCalories(double BMR, String activityLevel, double weightGoal) {
+        double calories = 0;
+        switch (activityLevel) {
+            case "Sedentary": calories = BMR * 1.2;
+                break;
+            case "Lightly Active": calories = BMR * 1.375;
+                break;
+            case "Moderately Active": calories = BMR * 1.55;
+                break;
+            case "Very Active": calories = BMR * 1.725;
+                break;
+        }
+        calories = (int)calories;
+        return calories + (500 * weightGoal);
     }
     
     // Getters and Setters
@@ -100,11 +115,11 @@ public class User {
         this.age = age;
     }
 
-    public Sex getSex() {
+    public String getSex() {
         return sex;
     }
 
-    public void setSex(Sex sex) {
+    public void setSex(String sex) {
         this.sex = sex;
     }
 
@@ -116,12 +131,12 @@ public class User {
         this.photoPath = photoPath;
     }
 
-    public int getZipCode() {
-        return zipCode;
+    public String getCity() {
+        return city;
     }
 
-    public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
+    public void setCity(String city) {
+        this.city = city;
     }
 
     public String getCountry() {
@@ -164,11 +179,11 @@ public class User {
         this.BMR = BMR;
     }
 
-    public ActiveLevel getActiveLevel() {
+    public String getActiveLevel() {
         return activeLevel;
     }
 
-    public void setActiveLevel(ActiveLevel activeLevel) {
+    public void setActiveLevel(String activeLevel) {
         this.activeLevel = activeLevel;
     }
 
