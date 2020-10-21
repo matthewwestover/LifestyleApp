90b20b04081a7023816be121cadd7dbf3e618f63
Matt <matthew.westover@gmail.com>
2020-10-21 15:24:00 -0600

    Start Steps

---
+++ WAMApp/app/src/main/java/com/example/wamapp/StepCounterFragment.java
@@ -0,0 +1,61 @@
+package com.example.wamapp;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+public class StepCounterFragment extends Fragment {
+
+    private TextView mTvBMIData;
+    private UserViewModel mUserViewModel;
+
+    public StepCounterFragment() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    final Observer<User> userObserver  = new Observer<User>() {
+        @Override
+        public void onChanged(@Nullable final User user) {
+            if(user!=null) {
+                double bmiValue = user.getBMI();
+                String bmiValueString = Double.toString(bmiValue);
+                mTvBMIData.setText(bmiValueString);
+            }
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.step_fragment, container, false);
+        mTvBMIData = view.findViewById(R.id.tv_step_data);
+
+        mUserViewModel = ViewModelProviders.of(getActivity()).get(UserViewModel.class);
+        mUserViewModel.getUser().observe(this, userObserver);
+
+        double bmiValue = 0.0;
+        String bmiValueString = Double.toString(bmiValue);
+
+        mTvBMIData.setText(bmiValueString);
+
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
